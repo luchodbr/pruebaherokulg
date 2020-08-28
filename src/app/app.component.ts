4a014d27153f5ffb9b89a573cbb5d044fec635d3
unknown <luchodbr@gmail.com>
2020-08-27 22:27:10 -0300

    ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab4PrimerProject';
+  public edad1 : number ;
+  public edad2 : number;
+  public suma :number;
+  public promedio:number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Calcular() {
+    this.suma = this.edad1 + this.edad2;
+    this.promedio = (this.edad2 + this.edad1) / 2 ;
+  }
+  LimpiarCuadro(){
+    this.edad1 = null;
+    this.edad2 = null;
+    this.suma = null;
+    this.promedio = null;
+   }
 }
